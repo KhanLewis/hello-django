64226f4579093b8edf846b2d78478b2d5091ca41
KhanLewis <khanlewis@hotmail.co.uk>
2023-01-23 11:23:01 +0000

    made SK safe to commit

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -1,4 +1,7 @@
 import os
+if os.path.exists("env.py"):
+    import env
+
 """
 Django settings for django_todo project.
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s==zjj6esl(t0hzzn8i0l0sw)t41$4+nlfyxa=ly-mv9zyd4aa'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
